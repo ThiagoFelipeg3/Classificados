24c6a146dcd1e2bb015f254c3f253c546335d84e
Thiago Felipe P. de Oliveira <38959275+ThiagoFelipeg3@users.noreply.github.com>
2018-08-28 09:49:14 -0300

    Update index.php

--- classificados_estruturado/index.php
+++ classificados_estruturado/index.php
@@ -1,7 +1,11 @@
 <?php require 'pages/header.php'; ?>
-<?php
+<?php 
+if(!isset($_SESSION['cLogin'])):?>
+	<script type="text/javascript">window.location.href="login.php"</script>
+<?php endif;
 require 'classes/anuncios.class.php';
-// require 'classes/usuarios.class.php'; jÃ¡ estava sendo criada em header.php
+//O require do Usuario jÃ¡ esta sendo criado em header.php
+// require 'classes/usuarios.class.php'; 
 require 'classes/categorias.class.php';
 $a = new Anuncios();
 $u = new Usuarios();
@@ -33,13 +37,13 @@ $categorias = $c->getLista();
 
 <div class="container-fluid">
 	<div class="jumbotron">
-		<h2>Nós temos hoje <?php echo $total_anuncios; ?> anúncios.</h2>
-		<p>E mais de <?php echo $total_usuarios; ?> usuários cadastrados.</p>
+		<h2>NÃ³s temos hoje <?php echo $total_anuncios; ?> anÃºncios.</h2>
+		<p>E mais de <?php echo $total_usuarios; ?> usuÃ¡rios cadastrados.</p>
 	</div>
 
 	<div class="row">
 		<div class="col-sm-3">
-			<h4>Pesquisa Avançada</h4>
+			<h4>Pesquisa AvanÃ§ada</h4>
 			<form method="GET">
 				<div class="form-group">
 					<label for="categoria">Categoria:</label>
@@ -52,7 +56,7 @@ $categorias = $c->getLista();
 				</div>
 
 				<div class="form-group">
-					<label for="preco">Preço:</label>
+					<label for="preco">PreÃ§o:</label>
 					<select id="preco" name="filtros[preco]" class="form-control">
 						<option></option>
 						<option value="0-50" <?php echo ($filtros['preco']=='0-50')?'selected="selected"':''; ?>>R$ 0 - 50</option>
@@ -63,12 +67,12 @@ $categorias = $c->getLista();
 				</div>
 
 				<div class="form-group">
-					<label for="estado">Estado de Conservação:</label>
+					<label for="estado">Estado de ConservaÃ§Ã£o:</label>
 					<select id="estado" name="filtros[estado]" class="form-control">
 						<option></option>
 						<option value="0" <?php echo ($filtros['estado']=='0')?'selected="selected"':''; ?>>Ruim</option>
 						<option value="1" <?php echo ($filtros['estado']=='1')?'selected="selected"':''; ?>>Bom</option>
-						<option value="2" <?php echo ($filtros['estado']=='2')?'selected="selected"':''; ?>>Ótimo</option>
+						<option value="2" <?php echo ($filtros['estado']=='2')?'selected="selected"':''; ?>>Ã“timo</option>
 					</select>
 				</div>
 
@@ -79,7 +83,7 @@ $categorias = $c->getLista();
 
 		</div>
 		<div class="col-sm-9">
-			<h4>Ultimos Anúncios</h4>
+			<h4>Ultimos AnÃºncios</h4>
 			<table class="table table-striped">
 				<tbody>
 					<?php foreach($anuncios as $anuncio): ?>
